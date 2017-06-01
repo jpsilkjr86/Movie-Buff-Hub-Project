3043f7170c0310ff3f533f7450ae77cb28ef4538
Tyler Peterson <peterbilt42@Tylers-MacBook-Pro.local>
2017-05-31 20:21:04 -0700

    added firebase

---
+++ assets/javascript/database-mng.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCT2tMPzSTWMUFnLWpw2SSuumErHTPEvds",
+    authDomain: "movie-buff-hub-project.firebaseapp.com",
+    databaseURL: "https://movie-buff-hub-project.firebaseio.com",
+    projectId: "movie-buff-hub-project",
+    storageBucket: "movie-buff-hub-project.appspot.com",
+    messagingSenderId: "905481455751"
+  };
+  firebase.initializeApp(config);
