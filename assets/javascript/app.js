31ac52712a3326ad4f14ac164b5aa9ef9de1cfb6
John Silkey <John Silkey>
2017-06-01 20:06:02 -0700

    wrapped doc event listeners in document.ready function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,20 @@
-// event listener for clicking submit
-$('#search-submit').on('click', function(e){
-	// prevents page from auto-reloading
-	e.preventDefault();
-	// testing testing 123
-	// saves main search query as variable
-	var searchQuery = $('#main-search').val().trim();
-	// empties input field
-	$('#main-search').val('');
+$(document).ready(function(){
+	// event listener for clicking submit
+	$('#search-submit').on('click', function(event){
+		// prevents page from auto-reloading
+		event.preventDefault();
+		// testing testing 123
+		// saves main search query as variable
+		var searchQuery = $('#main-search').val().trim();
+		// empties input field
+		$('#main-search').val('');
 
-	searchOMDBbyMovie(searchQuery);
-});
+		searchOMDBbyMovie(searchQuery);
+	});
 
-// event listener for pressing ENTER key when in the input field
-$('#main-search').on('keypress', function(e){
-	// if the key is ENTER, trigger 'click' event on #search-submit
-	if (e.which === 13) {$('#search-submit').trigger('click');}
+	// event listener for pressing ENTER key when in the input field
+	$('#main-search').on('keypress', function(event){
+		// if the key is ENTER, trigger 'click' event on #search-submit
+		if (event.which === 13) {$('#search-submit').trigger('click');}
+	});
 });
