57472a620dafd7c5c70c8a449e0c181fbe9d2d8e
John Silkey <jpsilkjr86@gmail.com>
2017-06-07 19:35:01 -0700

    changed methods.js to client-methods.js and moved server-side methods to server-methods.js

--- assets/javascript/client-methods.js
+++ assets/javascript/client-methods.js
@@ -1,11 +1,3 @@
-// function to get a user key (unique id) by generating one on firebase
-function generateFirebaseUserKey() {
-	var userRef = firebase.database().ref('usersearches');
-	var gotKey = userRef.push().key;
-	gotKey = 'user_' + gotKey; // adds 'user_' to the beginning of the string
-	return gotKey;
-}
-
 // function to set user key in local storage
 function setUserKey(keyArg) {
 	localStorage.setItem('User Key', keyArg);
@@ -29,14 +21,6 @@ function getUserKey() {
 	// always returns the key at the end of the function
 	console.log('User Key:', k);
 	return k;
-}
-
-// function to get a key (unique id) for a search entry by generating one on firebase
-function generateFirebaseSearchKey() {
-	var searchesRef = firebase.database().ref('allsearches');
-	var gotKey = searchesRef.push().key;
-	gotKey = 'search_' + gotKey; // adds 'user_' to the beginning of the string
-	return gotKey;
 }
 
 // function for creating a movie-suggestion div
@@ -90,25 +74,6 @@ function isSearchInputValid(query) {
 	if (query != '' && query != null) {
 		console.log(true); return true;
 	}
-}
-
-// object constructur that returns a search data object. some of the data object's
-// properties will be updated after the ajax request is complete.
-function searchDataObject(query, queryType) {
-	this.query = query;
-	this.queryType = queryType;
-	this.id = getUserKey();
-	this.timestamp = 0;
-	this.results = {};
-}
-
-// function to write search entry data to both 'allusers' and 'usersearches' firebase directories
-function writeSearchData(searchObject, searchKey) {
-	// saves timestamp
-	searchObject.timestamp = firebase.database.ServerValue.TIMESTAMP;
-	// writes same data to two separate references in firebase
-	database.ref('allsearches/' + searchKey).set(searchObject);
-	database.ref('usersearches/' + searchObject.id + '/' + searchKey).set(searchObject);
 }
 
 function getCarouselItem(imgURL){
