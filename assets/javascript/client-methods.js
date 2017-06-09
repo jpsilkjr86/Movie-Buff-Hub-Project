c6828e836dfc8cb44fd07e02a516cd921c177c12
John Silkey <jpsilkjr86@gmail.com>
2017-06-09 11:59:46 -0700

    changed carousel href #'s to integers instead of spelled-out numbers. Still works, much more extensible now!

--- assets/javascript/client-methods.js
+++ assets/javascript/client-methods.js
@@ -79,8 +79,8 @@ function isSearchInputValid(query) {
 function getCarouselItem(imgURL){
 	var item = $('<a>');
 
-	item.addClass('carousel-item')
-		.attr('href', '#' + getCarouselIndex() + '!');
+	item.addClass('carousel-item') // carouselIndex is a global variable (see database-mng.js)
+		.attr('href', '#' + carouselIndex + '!');
 
 	var poster = $('<img>');
 
@@ -88,71 +88,6 @@ function getCarouselItem(imgURL){
 		.appendTo(item);
 
 	 return item;
-}
-
-function getCarouselIndex() {
-	switch (carouselIndex) {
-		case 1:
-			return 'one';
-			break;
-		case 2:
-			return 'two';
-			break;
-		case 3:
-			return 'three';
-			break;
-		case 4:
-			return 'four';
-			break;
-		case 5:
-			return 'five';
-			break;
-		case 6:
-			return 'six';
-			break;
-		case 7:
-			return 'seven';
-			break;
-		case 8:
-			return 'eight';
-			break;
-		case 9:
-			return 'nine';
-			break;
-		case 10:
-			return 'ten';
-			break;
-		case 11:
-			return 'eleven';
-			break;
-		case 12:
-			return 'twelve';
-			break;
-		case 13:
-			return 'thirteen';
-			break;
-		case 14:
-			return 'fourteen';
-			break;
-		case 15:
-			return 'fifteen';
-			break;
-		case 16:
-			return 'sixteen';
-			break;
-		case 17:
-			return 'seventeen';
-			break;
-		case 18:
-			return 'eighteen';
-			break;
-		case 19:
-			return 'nineteen';
-			break;
-		case 20:
-			return 'twenty';
-			break;
-	}
 }
 
 function getSmallMovieCard(result) {
